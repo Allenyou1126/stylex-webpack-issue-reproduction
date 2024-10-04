31a4c1fbc9e7e078b44e1412caf7f973f965df5a
Allenyou <i@allenyou.wang>
2024-10-04 19:27:53 +0800

    创建最小复现

---
+++ next.config.cjs
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { withStyleX } = require("stylex-webpack/next");
+
+module.exports = withStyleX({
+	// stylex-webpack options goes here, see the following section for more details
+})({
+	// Your Next.js config goes here.
+	reactStrictMode: true,
+});
